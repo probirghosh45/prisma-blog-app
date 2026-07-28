fd0fe97dd771b68413d4133025459ee0083b7c53
Probir Ghosh <probirghosh@outlook.com>
2026-07-28 09:46:12 +0600

    :rocket: initial configuration done

---
+++ cmd.tsx
@@ -0,0 +1,12 @@
+// npm init
+// npm install typescript tsx @types/node --save-dev
+// npx tsc --init
+
+// npm install prisma @types/pg --save-dev
+// npm install @prisma/client @prisma/adapter-pg pg dotenv
+
+// npx prisma init --output ../generated/prisma
+
+// npm add express cors
+
+
