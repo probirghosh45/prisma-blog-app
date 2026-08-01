bd127a45f6c03e1b1599c0cbf8adfd689b99c513
Probir Ghosh <probirghosh@outlook.com>
2026-08-01 23:28:03 +0600

    :tada: refactor: simplify authentication middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,6 +1,6 @@
-import { Request, Response, NextFunction } from "express";
-import { auth as betterAuth } from "../lib/auth";
 import { fromNodeHeaders } from "better-auth/node";
+import { NextFunction, Request, Response } from "express";
+import { auth as betterAuth } from "../lib/auth";
 
 export enum UserRole {
   Admin = "ADMIN",
@@ -23,46 +23,50 @@ declare global {
 
 const auth = (...roles: UserRole[]) => {
   return async (req: Request, res: Response, next: NextFunction) => {
-    // console.log("Authenticating user...");
-    // console.log(roles)
-    const session = await betterAuth.api.getSession({
-      headers: fromNodeHeaders(req.headers),
-    });
-    // console.log("Session:", session);
-    // return res.json(session);
-
-    if (!session) {
-      return res.status(401).json({
-        success: false,
-        message: "Unauthorized",
+    try {
+      // console.log("Authenticating user...");
+      // console.log(roles)
+      const session = await betterAuth.api.getSession({
+        headers: fromNodeHeaders(req.headers),
       });
+      // console.log("Session:", session);
+      // return res.json(session);
+
+      if (!session) {
+        return res.status(401).json({
+          success: false,
+          message: "Unauthorized",
+        });
+      }
+
+      if (!session.user.emailVerified) {
+        return res.status(403).json({
+          success: false,
+          message: "Email not verified,Please verify your email",
+        });
+      }
+
+      req.user = {
+        id: session.user.id,
+        name: session.user.name,
+        email: session.user.email,
+        role: session.user.role as string,
+        emailVerified: session.user.emailVerified,
+      };
+
+      if (roles.length && !roles.includes(req.user.role as UserRole)) {
+        return res.status(403).json({
+          success: false,
+          message:
+            "Forbidden, You don't have permission to access this resource",
+        });
+      }
+      // console.log(session);
+
+      next();
+    } catch (error) {
+      next(error);
     }
-
-    if (!session.user.emailVerified) {
-      return res.status(403).json({
-        success: false,
-        message: "Email not verified,Please verify your email",
-      });
-    }
-
-    req.user = {
-      id: session.user.id,
-      name: session.user.name,
-      email: session.user.email,
-      role: session.user.role as string,
-      emailVerified: session.user.emailVerified,
-    };
-
-    if (roles.length && !roles.includes(req.user.role as UserRole)) {
-      return res.status(403).json({
-        success: false,
-        message: "Forbidden, You don't have permission to access this resource",
-      });
-    }
-    // console.log(session);
-
-    next();
   };
 };
-
 export default auth;
