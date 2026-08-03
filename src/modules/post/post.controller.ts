c2f9884a4a1c5d0e07f482dbe48b4db406658ead
Probir Ghosh <probirghosh@outlook.com>
2026-08-03 23:55:34 +0600

    :clown_face: working on query by title: fake commit

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,27 +1,32 @@
 import { Request, Response } from "express";
 import { PostService } from "./post.service";
 
-const createPost = async (req:Request, res:Response) => {
-    try {
-        // console.log("check",req.user)
-        const result = await PostService.createPost(req.body, req.user?.id as string);
-        res.status(201).json(result);
-    } catch (error) {
-        res.status(500).json({ error: "Failed to create post" });
-    }
-}
+const createPost = async (req: Request, res: Response) => {
+  try {
+    // console.log("check",req.user)
 
-const getAllPosts = async (req:Request, res:Response) => {
-    try {
-        const result = await PostService.getAllPosts();
-        res.status(200).json(result);
-    }
-    catch (error) {
-        res.status(500).json({ error: "Failed to fetch posts" });
-    }
-}
+    const result = await PostService.createPost(
+      req.body,
+      req.user?.id as string,
+    );
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to create post" });
+  }
+};
+
+const getAllPosts = async (req: Request, res: Response) => {
+  try {
+    const { search } = req.query;
+    console.log("search value", search);
+    const result = await PostService.getAllPosts();
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch posts" });
+  }
+};
 
 export const PostController = {
-    createPost,
-    getAllPosts
-}
+  createPost,
+  getAllPosts,
+};
